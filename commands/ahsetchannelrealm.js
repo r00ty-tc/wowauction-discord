cc1afb5516b2dcdf16d8e5419eb125c68b5fce67
r00ty-tc <r00ty-tc@users.noreply.github.com>
2023-11-19 21:17:40 +0000

    Updated set realm/set channel realm to include Onyxia and add realm numbers to description

--- commands/ahsetchannelrealm.js
+++ commands/ahsetchannelrealm.js
@@ -3,11 +3,11 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
         data: new SlashCommandBuilder()
                 .setName('ahsetchannelrealm')
-                .setDescription('Sets the realm to use on this specific channel (Lordaeron,Icecrown)')        
+                .setDescription('Sets the realm to use on this specific channel (14=Lordaeron,15=Icecrown,17=Onyxia)')
                 .addStringOption(option =>
                         option
                                 .setName('realm')
-                                .setDescription('The realm to use on this server (Lordaeron,Icecrown)')
+                                .setDescription('The realm to use on this specific channel (14=Lordaeron,15=Icecrown,17=Onyxia)')
                                 .setRequired(true))
                 .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
 };
--- commands/ahsetrealm.js
+++ commands/ahsetrealm.js
@@ -3,11 +3,11 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
         data: new SlashCommandBuilder()
                 .setName('ahsetrealm')
-                .setDescription('Sets the realm to use on this discord server (Lordaeron,Icecrown)')        
+                .setDescription('Sets the realm to use on this discord server (14=Lordaeron,15=Icecrown,17=Onyxia)')
                 .addStringOption(option =>
                         option
                                 .setName('realm')
-                                .setDescription('The realm to use on this server (Lordaeron,Icecrown)')
+                                .setDescription('The realm to use on this server (14=Lordaeron,15=Icecrown,17=Onyxia)')
                                 .setRequired(true))
                 .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
 };
